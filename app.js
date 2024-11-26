6a3f96b65217ab921397165b0e247b471421670b
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-26 12:26:57 -0300

    Filter by id and genre endpoints

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
   res.json(movies)
 })
 
 app.get('/movies/:id', (req, res) => {
-    
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) {
+    res.json(movie)
+  }
+  res.status(404).json({ message: '404 Movie not found' })
 })
 
 const PORT = process.env.PORT ?? 3000;
