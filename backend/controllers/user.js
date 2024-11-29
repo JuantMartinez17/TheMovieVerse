bb1d103c55e7443da4c5944a86ee35b59e40ad26
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-29 10:33:33 -0300

    users getAll and getById working

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,7 +5,7 @@ import p from 'picocolors';
 export class UsersController{
     static async getAll(req, res) {
         try{
-            users = await UsersModel.getAll()
+            const users = await UsersModel.getAll()
             if (!users || users.length === 0) {
                 return res.status(404).json({ error: 'Users not found' });
             }
@@ -15,18 +15,21 @@ export class UsersController{
             res.status(500).json({ message: 'Internal server error' })
         }
     }
+
     static async getById(req, res) {
         try {
-            const { id } = req.params
-            const userId = Number(id)
-            const user = await UsersModel.getById({ userId })
-            if (!user) {
-                return res.status(404).json({ error: 'User not found' });
+            const { id } = req.params;
+            const userId = Number(id);
+          
+            const { error, user } = await UsersModel.getById({ id: userId });
+          
+            if (error) {
+            return res.status(error.code).json({ message: error.message });
             }
-            res.status(200).json(user)
+            res.status(200).json(user);
         } catch (error) {
-            console.error(p.red(`Error fetching user: ${error.message}`))
-            res.status(500).json({ message: 'Internal server error' })
+            console.error(p.red(`Error fetching user: ${error.message}`));
+            res.status(500).json({ message: 'Internal server error' });
         }
     }
     static async create (req, res) {
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,24 @@
-export class UsersModel{}
+import { User } from '../database/config.js'
+
+export class UsersModel{
+    static async getAll() {
+        const users = await User.findAll()
+        return { users }
+    }
+    static async getById({ id }) {
+        try {
+          const userId = Number(id);
+          const userDetail = await User.findByPk(userId);
+          console.log('User id:', id);
+          
+          if (!userDetail) {
+           return { error: { code: 404, message: 'User not found' }, user: null };
+          }
+          
+          return { error: null, user: userDetail };
+        } catch (error) {
+          console.error('Error getting user by id:', error);
+          return { error: { code: 500, message: 'Internal server error' }, user: null };
+        }
+    }
+}
