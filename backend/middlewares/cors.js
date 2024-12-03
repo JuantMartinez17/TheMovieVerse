fe5e762024f54a1c5c15386650a1389b9e6451f8
JuantMartinez17 <juantomasm0@gmail.com>
2024-12-03 13:10:55 -0300

    cors changes

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,7 +2,8 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
   'http://localhost:8080',
   'http://localhost:3000',
-  'http://localhost:3006'
+  'http://localhost:3006',
+  'http://localhost:3007',
 ]
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
   origin: (origin, callback) => {
