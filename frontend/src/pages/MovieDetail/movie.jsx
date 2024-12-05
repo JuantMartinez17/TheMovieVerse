fa8381aead0c5cc7975765675e87fe197eb5880d
JuantMartinez17 <juantomasm0@gmail.com>
2024-12-05 11:19:48 -0300

    Movie detail page was using old backend url, fixed an error with navbar navigation

--- frontend/src/pages/MovieDetail/movie.jsx
+++ frontend/src/pages/MovieDetail/movie.jsx
@@ -20,7 +20,7 @@ const Movie = () => {
   useEffect(() => {
     const fetchMovieDetails = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/movies/${id}`);
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/movies/${id}`);
         const { error, movieDetail } = response.data;
         if (error) {
           setError(error);
